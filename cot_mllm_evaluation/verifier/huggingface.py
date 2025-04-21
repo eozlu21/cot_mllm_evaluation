2a5320315f65611a3fdf9f61a8344e7a5f2b1cd2
Ege Erdem Özlü <eozlu21@ku.edu.tr>
2025-04-21 03:08:41 +0300

    chimpanzini bananini

--- cot_mllm_evaluation/verifier/huggingface.py
+++ cot_mllm_evaluation/verifier/huggingface.py
@@ -31,6 +31,6 @@ class LLMVerifier(BaseVerifier):
         encoded = self.tokenizer(prompt, return_tensors="pt").to(self.device)
         generated = self.model.generate(**encoded, max_new_tokens=1)
         print(f"Prompt provided to verifier: {prompt}")
-        print(f"Generated answer by verifier: {generated}")
         answer = self.tokenizer.decode(generated[0], skip_special_tokens=True).lower()
+        print(f"Generated answer by verifier: {answer}")
         return "yes" in answer
